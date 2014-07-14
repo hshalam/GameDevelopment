a97486bc6625a63a84b41baa8abb2361dfcc15a0
Haley Shalam <hs2590@nyu.edu>
2014-07-14 14:46:29 -0400

    unity save folder

---
+++ game develpment/homework 1/Assets/PoemManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoemManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+		Debug.Log ("Hello World");
+
+		GetComponent<TextMesh>().text = "See the snow\nIts a blanket, soft and white\nFalling slow\nCovering the world, making it bright";
+	} 
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
